517acbc99dabdb8b63457741716f2aeeffe91253
Daniel Samorro <daniel.samorro@gmail.com>
2022-09-12 14:40:38 -0300

    add solution and api project

---
+++ BookingService/BookingService.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        public HealthCheckController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
